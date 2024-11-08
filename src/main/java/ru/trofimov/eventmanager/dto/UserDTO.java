86720928b4b83729d43980d5c6c6818686f1126d
Pavel <p.al.trofimov@gmail.com>
2024-11-08 15:01:21 +0300

    bug fix

--- src/main/java/ru/trofimov/eventmanager/dto/UserDTO.java
+++ src/main/java/ru/trofimov/eventmanager/dto/UserDTO.java
@@ -3,7 +3,6 @@ package ru.trofimov.eventmanager.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import ru.trofimov.eventmanager.enums.Role;
 
 public record UserDTO(
